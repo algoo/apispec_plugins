20333b6645d9183530e19a74361f59f61256aa93
Bastien Sevajol <bastien.sevajol@algoo.fr>
2018-12-03 11:01:57 +0100

    Display a prevent message about renaming

--- apispec_hapic_marshmallow/setup.py
+++ apispec_hapic_marshmallow/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="apispec_hapic_marshmallow",
-    version="0.2",
+    version="0.2.1",
     author="Algoo",
     author_email="contact@algoo.fr",
     description="Marshmallow apispec plugin for hapic",
@@ -11,7 +11,8 @@ setup(
     keywords="apispec openapi marshmallow api",
     url="https://github.com/algoo/apispec_plugins",
     packages=['apispec_hapic_marshmallow'],
-    long_description='https://github.com/algoo/apispec_plugins/tree/master/apispec_hapic_marshmallow',
+    long_description='This is the obsolete plugin, please use '
+                     'apispec-marshmallow-advanced instead',
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3.4",
