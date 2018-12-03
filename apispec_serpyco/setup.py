af84fa8180b18947ca37b73d1103c6d5337bbfd2
Bastien Sevajol <bastien.sevajol@algoo.fr>
2018-12-03 11:42:32 +0100

    be compatible with serpyco 0.16.1

--- apispec_serpyco/setup.py
+++ apispec_serpyco/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="apispec_serpyco",
-    version="0.5",
+    version="0.6",
     author="Algoo",
     author_email="contact@algoo.fr",
     description="Serpyco plugin for Apispec",
