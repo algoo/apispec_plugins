6716e117b50deb4e30505d0688b325738b9d4fd4
Bastien Sevajol <bastien.sevajol@algoo.fr>
2018-12-03 10:52:52 +0100

    Code adaptation for Bangertm refactor-nested branch

--- apispec_marshmallow_advanced/apispec_marshmallow_advanced/__init__.py
+++ apispec_marshmallow_advanced/apispec_marshmallow_advanced/__init__.py
@@ -1,51 +1,19 @@
 # coding: utf-8
-from marshmallow import fields
 from apispec.ext.marshmallow import MarshmallowPlugin
 
-from apispec_marshmallow_advanced.common import schema_class_resolver
+from apispec_marshmallow_advanced.common import generate_schema_name
 from apispec_marshmallow_advanced.openapi import HapicOpenAPIConverter
 
 
 class MarshmallowAdvancedPlugin(MarshmallowPlugin):
+    def __init__(self, schema_name_resolver=None):
+        schema_name_resolver = schema_name_resolver or generate_schema_name
+        super().__init__(schema_name_resolver)
+
     def init_spec(self, spec):
         super().init_spec(spec)
         self.openapi = HapicOpenAPIConverter(
-            openapi_version=spec.openapi_version, spec=self.spec
+            openapi_version=spec.openapi_version,
+            spec=self.spec,
+            schema_name_resolver=self.schema_name_resolver,
         )
-
-    def inspect_schema_for_auto_referencing(self, original_schema_instance):
-        """Override of apispec.ext.marshmallow.MarshmallowPlugin to be able to
-        use our schema_class_resolver (generate class with care of exclude/only)
-
-        FIXME BS 2018-10-26: This is a complete copy-paste to permit usage of
-        schema_class_resolver. Make a PR to make this method more overload
-        friend.
-
-        :param original_schema_instance: schema to parse
-        """
-        # schema_name_resolver must be provided to use this function
-        assert self.schema_name_resolver
-
-        for field in original_schema_instance.fields.values():
-            nested_schema_class = None
-
-            if isinstance(field, fields.Nested):
-                # THIS IS THE CHANGED LINE
-                # nested_schema_class = self.spec.schema_class_resolver(
-                nested_schema_class = schema_class_resolver(self.spec, field.schema)
-
-            elif isinstance(field, fields.List) and isinstance(
-                field.container, fields.Nested
-            ):
-                # THIS IS THE CHANGED LINE
-                # nested_schema_class = self.spec.schema_class_resolver(
-                nested_schema_class = schema_class_resolver(
-                    self.spec, field.container.schema
-                )
-
-            if nested_schema_class and nested_schema_class not in self.openapi.refs:
-                definition_name = self.schema_name_resolver(nested_schema_class)
-                if definition_name:
-                    self.spec.components.schema(
-                        definition_name, schema=nested_schema_class
-                    )
