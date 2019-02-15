3a1a6bc975e75ff605241c3923d08da0bee85eee
Bastien Sevajol <bastien.sevajol@algoo.fr>
2019-02-15 17:02:03 +0100

    bump version to 0.11

--- apispec_serpyco/setup.py
+++ apispec_serpyco/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="apispec_serpyco",
-    version="0.10",
+    version="0.11",
     author="Algoo",
     author_email="contact@algoo.fr",
     description="Serpyco plugin for Apispec",
